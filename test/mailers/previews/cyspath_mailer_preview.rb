3f70331bb553aa6082b5643fa321430e10486827
Mike <lvlichaelly@gmail.com>
2015-10-11 15:15:37 -0700

    added figaro for sensitive information safekeeping

--- test/mailers/previews/cyspath_mailer_preview.rb
+++ test/mailers/previews/cyspath_mailer_preview.rb
@@ -1,4 +1,10 @@
 # Preview all emails at http://localhost:3000/rails/mailers/cyspath_mailer
 class CyspathMailerPreview < ActionMailer::Preview
 
+    def sample_mail_preview
+      CyspathMailer.sample_email(User.first)
+    end
+
+# http://localhost:3000/rails/mailers/example_mailer/sample_mail_preview
+
 end
