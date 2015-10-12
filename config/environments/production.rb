3a71dcb5e5413edd6673157046c609b90c1d4bf8
Mike <lvlichaelly@gmail.com>
2015-10-11 19:47:55 -0700

    pushed to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,8 +6,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :user_name            => ENV['gmail_username'],
-   :password             => ENV['gmail_password'],
+   :user_name            => 'appointment.mailer@gmail.com',
+   :password             => 'cyspathe1',
    :authentication       => "plain",
   :enable_starttls_auto => true
   }
